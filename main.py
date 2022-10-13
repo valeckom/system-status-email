376c3e6a65dcd897e6f9dee9a1fe5567df85e30d
Mark Valecko <valeckom@gmail.com>
2022-10-13 08:49:25 -0700

    send html email

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 import smtplib
 from email.message import EmailMessage
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 from dotenv import load_dotenv
 
@@ -8,7 +10,7 @@ _ = load_dotenv()
 
 
 # From https://mljar.com/blog/python-send-email/
-def send_email(to, subject, message):
+def send_email(to, subject, message, alt_html):
     try:
         email_address = os.environ.get("EMAIL_ADDRESS")
         email_password = os.environ.get("EMAIL_PASSWORD")
@@ -21,10 +23,26 @@ def send_email(to, subject, message):
 
         # create email
         msg = EmailMessage()
+        # msg['Subject'] = subject
+        # msg['From'] = email_address
+        # msg['To'] = to
+        # msg.set_content(message)
+        # msg.add_alternative(alt_html)
+
+        msg = MIMEMultipart('alternative')
         msg['Subject'] = subject
         msg['From'] = email_address
         msg['To'] = to
-        msg.set_content(message)
+
+        # Record the MIME types of both parts - text/plain and text/html.
+        part1 = MIMEText(message, 'plain')
+        part2 = MIMEText(alt_html, 'html')
+
+        # Attach parts into message container.
+        # According to RFC 2046, the last part of a multipart message, in this case
+        # the HTML message, is best and preferred.
+        msg.attach(part1)
+        msg.attach(part2)
 
         # send email
         with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
@@ -37,4 +55,18 @@ def send_email(to, subject, message):
     return False
 
 
-send_email("mark86v@gmail.com", "Test", "Hello world!")
+send_email("mark86v@gmail.com", "Test", "Hello world!", """\
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html lang="en">
+<head>
+    <title>Title</title>
+    <meta name="format-detection" content="date=no">
+    <meta name="format-detection" content="telephone=no">
+    <meta content="width=device-width, initial-scale=1" name="viewport"/>
+    <style type="text/CSS"></style>
+</head>
+<body>
+<h1>Test Email</h1>
+<p>Hello world!</p>
+</body>
+</html>""")
